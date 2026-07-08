45c427c0b46b070869e8b0e02ada8a392ab46501
David <atunwadavidb@gmail.com>
2026-07-08 15:32:32 +0100

    what...

--- backend/app/models/Kanji.py
+++ backend/app/models/Kanji.py
@@ -6,12 +6,15 @@ from ..models import Base
 
 class Kanji(Base):
     __tablename__ = "kanji"
-    id : Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    id : Mapped[int] = mapped_column(primary_key=True, 	autoincrement=True)
     character : Mapped[str] = mapped_column(String)
     meaning: Mapped[str]
-    onyomi : Mapped[str]
-    kunyomi : Mapped[str | None]
-    radicals : Mapped[str | None] 
+    onyomi_katakana : Mapped[str]
+    onyomi_romaji : Mapped[str]
+    kunyomi_hiragana : Mapped[str | None]
+    kunyomi_romaji : Mapped[str | None]
+    radical : Mapped[str | None]
+    radical_meaning = Mapped[str | None]
     stroke_count : Mapped[int]
     jlpt_level : Mapped[int]
     curriculumn_order : Mapped[int]
