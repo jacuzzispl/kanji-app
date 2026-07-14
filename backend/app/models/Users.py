2eceaeeb6043a72c951a1e8ecd5ca8bc7b522624
David <atunwadavidb@gmail.com>
2026-07-14 18:28:34 +0100

    added default value for User model

--- backend/app/models/Users.py
+++ backend/app/models/Users.py
@@ -1,6 +1,6 @@
-from sqlalchemy import String, DateTime, Integer
+from sqlalchemy import String, DateTime, Integer, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from datetime import datetime
+from datetime import datetime, timezone
 from app.database import Base
 
 def get_username(context):
@@ -13,7 +13,7 @@ class User(Base):
     email : Mapped[str] = mapped_column(String(256), unique=True, index=True, nullable=False)
     password_hash : Mapped[str] = mapped_column(String(255), nullable=False)
     username : Mapped[str | None] = mapped_column(String(50), default=get_username)
-    created_at : Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    created_at : Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False, server_gdefault= func.now())
     rank : Mapped[int | None] = mapped_column(Integer, nullable=True)
 
     reviews = relationship("Review")
