9b1de6ce631fe4ec16c38f77cb64b536609789b2
David <atunwadavidb@gmail.com>
2026-08-26 13:32:18 +0100

    type hints for nullable fields in UserKanji were not nullable

--- backend/app/models/UserKanji.py
+++ backend/app/models/UserKanji.py
@@ -2,6 +2,7 @@ from sqlalchemy import ForeignKey, DateTime, String, Integer, Float, func
 from sqlalchemy.orm import relationship, Mapped, mapped_column
 from datetime import datetime 
 from .Base import Base
+from typing import Optional
 
 class UserKanjiEntry(Base):
     __tablename__ = "user_kanji"
@@ -12,9 +13,9 @@ class UserKanjiEntry(Base):
     interval_days : Mapped[float] = mapped_column(Float, default = 1.0) #how many days until next review is due
     times_reviewed : Mapped[int] = mapped_column(Integer, default = 0)
     times_correct : Mapped[int] = mapped_column(Integer, default=0)
-    last_reviewed_at : Mapped[datetime] = mapped_column(DateTime(timezone=True), default=func.now(), nullable= False)
-    next_review_at : Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable = True) #when the kanji is first loaded, they will not have studied so it can not be up for review
-    ease_factor : Mapped[float] = mapped_column(Float, nullable = True)
+    last_reviewed_at : Mapped[Optional[datetime]] = mapped_column(DateTime(timezone=True), nullable= True)
+    next_review_at : Mapped[Optional[datetime]] = mapped_column(DateTime(timezone=True), nullable = True) #when the kanji is first loaded, they will not have studied so it can not be up for review
+    ease_factor : Mapped[Optional[float]] = mapped_column(Float, nullable = True)
     created_at : Mapped[datetime] = mapped_column(DateTime(timezone=True), default=func.now(), nullable=False)
     
     user = relationship("User")
