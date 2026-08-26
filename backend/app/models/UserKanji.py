8f2ada9eb4a086ac2eeaa46a62cdcd78b2b747ed
David <atunwadavidb@gmail.com>
2026-08-26 12:44:42 +0100

    default for next_review and created_at for UserKanjiEntry

--- backend/app/models/UserKanji.py
+++ backend/app/models/UserKanji.py
@@ -9,13 +9,13 @@ class UserKanjiEntry(Base):
     user_id : Mapped[int] = mapped_column(Integer, ForeignKey("user_profiles.id"))
     kanji_id : Mapped[int] = mapped_column(ForeignKey("kanji.id"))
     status : Mapped[str] = mapped_column(String, nullable=False)
-    interval_days : Mapped[float] = mapped_column(Float) #1 equivalent to 1 day
+    interval_days : Mapped[float] = mapped_column(Float, default = 1.0) #how many days until next review is due
     times_reviewed : Mapped[int] = mapped_column(Integer, default = 0)
     times_correct : Mapped[int] = mapped_column(Integer, default=0)
     last_reviewed_at : Mapped[datetime] = mapped_column(DateTime(timezone=True), default=func.now(), nullable= False)
-    next_review_at : Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable = False)
+    next_review_at : Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable = True) #when the kanji is first loaded, they will not have studied so it can not be up for review
     ease_factor : Mapped[float] = mapped_column(Float, nullable = True)
-    created_at : Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    created_at : Mapped[datetime] = mapped_column(DateTime(timezone=True), default=func.now(), nullable=False)
     
     user = relationship("User")
     kanji = relationship("Kanji")
