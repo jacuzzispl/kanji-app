b2d4c483247b18c5a7c411cbb5b94bde9f9eece5
David <atunwadavidb@gmail.com>
2026-09-06 19:27:42 +0100

    enabled CORS in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import engine
 from .routers import auth
 
+origins = ["http://localhost:8000"]
 
 app = FastAPI()
+app.add_middleware(CORSMiddleware,
+                   allow_origins=origins,
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"])
+
+
+
 app.include_router(auth.router)
 
 @app.get("/")
